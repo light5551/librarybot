abd5999f3d6cd97bf9bd803753ba0e6fa77aeb10
Andrew <gavrilov.andrew1999@yandex.ru>
2020-06-09 21:42:14 +0300

    Test markdown parser and test books are added

--- src/import_book.py
+++ src/import_book.py
@@ -1,12 +1,13 @@
 import pypandoc
 from bs4 import BeautifulSoup
+CH = 'h2'
 
 class book:
-    filename = None
     html_book = None
-    ch = 'h2'
     def __init__(self, name):
         self.filename = name
+        self.read_from_epub()
+        self.open_html_book()
 
     def read_from_epub(self):
         new_filename = self.filename[:-4] + 'html'
@@ -14,11 +15,11 @@ class book:
         self.filename = new_filename
 
     def open_html_book(self):
-        with open(self.filename) as fp:
+        with open(self.filename, encoding='UTF-8') as fp:
             self.html_book = BeautifulSoup(fp, 'html.parser')
 
     def get_chapt(self, num, part = 0):
-        headers = self.html_book.find_all(self.ch)
+        headers = self.html_book.find_all(CH)
         chapter = [headers[num-1]]
         if num == len(headers):
             chapter += chapter[0].find_all_next()
@@ -35,5 +36,5 @@ class book:
             if length + len(el) < 4000 * (part + 1) and length > 4000 * part:
                 res.append(el)
                 length +=len(el)
-        return '\n\n'.join([el for el in res])
+        return '\n'.join([el for el in res])
 
